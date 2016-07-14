ff93d1146912d17573ca84c79b02f2391d305dc8
Casey <casey.bain@8451.com>
2016-07-14 08:23:35 +0500

    added sqlite connection to HON.db

--- switch.py
+++ switch.py
@@ -1,13 +1,24 @@
 import RPi.GPIO as GPIO
 import time
+import sqlite3 as lite
 #import tkMessageBox
+
+#sqlite_file = 'HON.sqlite'
+#table_name = 'tblHONResults'
+
+
+#connect to HON sqlite DB
+conn = lite.connect('HON.db')
+c = conn.cursor()
 
 GPIO.setmode(GPIO.BCM)
 
+#Bind pins to input & up position
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 while True:
     input_stateGreen = GPIO.input(18)
@@ -16,6 +27,7 @@ while True:
         print('GREEN Button Pressed')
         #tkMessageBox.showinfo(title="BUTTON RESPONSE", message="GREEN!")
         #time.sleep(2.0)
+        
         time.sleep(0.2)
 
     input_stateBlue = GPIO.input(4)
