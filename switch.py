81754d2ffecb619ba18c295bb3aad46c1a9c7a16
Casey <casey.bain@8451.com>
2016-07-18 12:41:24 -0400

    added mp3 response

--- switch.py
+++ switch.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import sqlite3 as lite
-#from PIL import Image
+import os
 
 
 #connect to HON sqlite DB
@@ -20,8 +20,6 @@ GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    #img = Image.open('Kroger_survey_question.png')
-    #img.show()
 
     input_stateGreen = GPIO.input(18)
     input_stateBlue = GPIO.input(4)
@@ -33,24 +31,27 @@ while True:
         c.execute("INSERT INTO tblHONResults ('HON_VALUE','LOYALTY_ID') VALUES (1,'NA')") #TODO add location attr (ie. store) & terminal id
         conn.commit()
         #print('Green Record written to DB.')
-        time.sleep(sleep_time)
+        os.system('mpg123 -q chime.mp3 &')
     
     if input_stateBlue == False:
         print('BLUE Button Pressed')
         c.execute("INSERT INTO tblHONResults ('HON_VALUE','LOYALTY_ID') VALUES (2,'NA')")
         conn.commit()
+        os.system('mpg123 -q chime.mp3 &')
         time.sleep(sleep_time)
 
     if input_stateYellow == False:
         print('YELLOW Button Pressed')
         c.execute("INSERT INTO tblHONResults ('HON_VALUE','LOYALTY_ID') VALUES (3,'NA')")
         conn.commit()
+        os.system('mpg123 -q chime.mp3 &')
         time.sleep(sleep_time)
     
     if input_stateRed == False:
         print('RED Button Pressed')
         c.execute("INSERT INTO tblHONResults ('HON_VALUE','LOYALTY_ID') VALUES (4,'NA')")
         conn.commit()
+        os.system('mpg123 -q chime.mp3 &')
         time.sleep(sleep_time)
 
 
